ccec3e7e93218c3ab46219fdb34a6c21d50b6dd8
deep-k7 <kondedeep73@gmail.com>
2026-06-28 16:04:58 +0530

    Add initial JavaScript setup for order, pricing, and extras

---
+++ script.js
@@ -0,0 +1,42 @@
+const order = {
+  drink: "espresso",
+  size: "medium",
+  milk: "regular",
+  extras: [],
+};
+
+const prices = {
+  espresso: 2.5,
+  latte: 3.8,
+  cappuccino: 3.6,
+  matcha: 4.0,
+  chai: 3.4,
+  small: 0.0,
+  medium: 0.5,
+  large: 1.0,
+  extrashot: 0.75,
+  vanilla: 0.6,
+  caramel: 0.6,
+  whipped: 0.5,
+};
+
+const drinkColours = {
+  espresso: "#1e0c04",
+  latte: "#7b4f2e",
+  cappuccino: "#b07442",
+  matcha: "#4a7c3f",
+  chai: "#b5812a",
+};
+
+const sizeHeight = {
+  small: "40%",
+  medium: "65%",
+  large: "90%",
+};
+
+const extraLabels = {
+  extrashot: "Extra Shot",
+  vanilla: "Vanilla Syrup",
+  caramel: "Caramel Syrup",
+  whipped: "Whipped Cream",
+};
